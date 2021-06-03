09b220a1afb0c8cb054e818dba440be2b26ce478
harikrsr <harikrishna3054@gmail.com>
2021-06-03 21:01:17 +0530

    CO4

---
+++ CO4/Q1/main.java
@@ -0,0 +1,21 @@
+package JAVA;
+import graphics.area;
+public class Main
+{
+
+	public static void main(String[] args) 
+	{
+		area ar=new area();
+		ar.rectangle();
+		ar.triangle();
+		ar.square();
+		ar.circle();
+	}
+     
+       
+      
+}
+
+
+	
+	
